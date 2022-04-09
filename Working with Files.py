33680d309687dd06f3a4e8e4ce8d0c6d43339266
Steven Sommer <steven.sommer@go.winona.edu>
2022-04-09 00:02:48 -0500

    Update Working with Files.py

--- Working with Files.py
+++ Working with Files.py
@@ -2,6 +2,10 @@
 # Author: Steven Sommer
 # Date: 2/18/2022
 
+#Problem
+#Pre-Condition: A file containing at most 1000 lines.
+#Post Condition: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
+
 i = 1
 f = open('rosalind_ini5.txt')
 for line in f.readlines():
