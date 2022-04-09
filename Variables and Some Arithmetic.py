2d08fc9432ffdf6980c2c9c4262c9ea5ad71b101
Steven Sommer <steven.sommer@go.winona.edu>
2022-04-09 00:07:25 -0500

    Update Variables and Some Arithmetic.py

--- Variables and Some Arithmetic.py
+++ Variables and Some Arithmetic.py
@@ -1,15 +1,13 @@
 # Problem: Variables and Some Arithmetic from ROSALIND
 # Author: Steven Sommer
-# Date: 2/18/2022
+# Date: 4/8/2022
 
-import math
 
-a = 836
-b = 898
-c = a ** 2
-d = b ** 2
-e = c + d
-print ('3 ^ 2 is: ', c)
-print ('5 ^ 2 is: ', d)
-print( c, "+", d, "=",e)
-print(math.sqrt(e))
+# Problem
+#Pre-Condition: Two positive integers a and b, each less than 1000.
+#Post Condition: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
+a = 819
+b = 976
+
+print (a ** 2 + b ** 2)
