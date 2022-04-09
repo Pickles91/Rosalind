e0625808903d1996831c18c15dcab8b21124be82
Steven Sommer <steven.sommer@go.winona.edu>
2022-04-09 00:12:34 -0500

    Update Counting DNA Nucleotides.py

--- Counting DNA Nucleotides.py
+++ Counting DNA Nucleotides.py
@@ -2,6 +2,14 @@
 # Author: Steven Sommer
 # Date: 2/18/2022
 
+# Problem:
+#  A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+#  An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+# Pre-Condition: A DNA string s of length at most 1000 nt.
+# Post Condition: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
+
 def count_bases1(s):
     a_count = 0
     c_count = 0
