5a66995858075cd898511196f9253d6399b2fa71
Steven Sommer <steven.sommer@go.winona.edu>
2022-04-09 00:08:28 -0500

    Update Installing Python .py

--- Installing Python .py
+++ Installing Python .py
@@ -2,4 +2,9 @@
 # Author: Steven Sommer
 # Date: 2/18/2022
 
+# Problem:
+# After downloading and installing Python, type import this into the Python command line and see what happens.
+# Then, click the "Download dataset" button below and copy the Zen of Python into the space provided.
+
+
 import this
