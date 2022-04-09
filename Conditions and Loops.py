15f9dd3fa3ecd8579b027c02156afa7e3ef8f6d0
Steven Sommer <steven.sommer@go.winona.edu>
2022-04-09 00:04:20 -0500

    Update Conditions and Loops.py

--- Conditions and Loops.py
+++ Conditions and Loops.py
@@ -2,10 +2,16 @@
 # Author: Steven Sommer
 # Date: 2/18/2022
 
+#Problem
+#Pre-Condition: Two positive integers a and b (a<b<10000).
+#Post Condition: The sum of all odd integers from a through b, inclusively.
+
 a = 4825
 b = 9741
 result = 0
+
 for i in range(a,b+1):
   if i % 2 == 1:
-    result += i
+   result += i
+   
 print (result)
